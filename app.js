eee995aa4d8c789afd37d0eff8a0cf26e76e6b2a
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-03-19 12:17:05 +0600

    function basic

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ var app = new Vue({
     data: {
         fname: "Emon",
         lname: "Hasan",
-    }
+    },
+
+    methods: {
+
+        test(){
+            return 'this is test method';
+        },
+        getvalue(){
+            return 'My name is '+this.fname+ ' '+this.lname;
+        }
+    },
 })
