ace07bfef98c296e289dffb9557750d11a36327c
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-03-19 16:43:39 +0600

    two way data binding example

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 var app = new Vue({
     el: '#our-app',
     data: {
-        x: 0,
-        y: 0,
-        counter: 0,
+        name: '',
     },
 
     methods: {
 
-        increment(){
-            this.counter++;
-        },
-
-        submitClick(){
-            alert('click');
-        }
+   
 
     },
 })
