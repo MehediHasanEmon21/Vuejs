9098315a67bd96ea9c4577cd210c6072d365693e
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-03-21 09:59:19 +0600

    concept of computing property

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var app = new Vue({
     el: '#our-app',
     data: {
-        name: '',
+        bangla: 0,
+        english: 0,
+        maths: 0,
     },
 
     methods: {
 
    
+
+    },
+    computed: {
+
+        totalMarks(){
+            return parseInt(this.bangla) +  parseInt(this.english) +  parseInt(this.maths)
+        }
 
     },
 })
