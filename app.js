60787a7d4f117df4bae716f1a729bfed93e80f2b
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-03-21 11:25:00 +0600

    v-for directive

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 var app = new Vue({
     el: '#our-app',
     data: {
-        count: 0,
+        students: ["A","B","C","d"],
+        teachers: [
+            {
+                name: 'Teacher 1',
+                subject: 'subject 1',
+                locations: {
+                    street: 'street 1',
+                    city: 'city 1'
+                }
+            },
+
+            {
+                name: 'Teacher 2',
+                subject: 'subject 2',
+                locations: {
+                    street: 'street 1',
+                    city: 'city 1'
+                }
+            },
+
+            {
+                name: 'Teacher 3',
+                subject: 'subject 3',
+                locations: {
+                    street: 'street 1',
+                    city: 'city 1'
+                }
+            },
+
+
+        ]
     },
 
     methods: {
 
-        increment(){
-            this.count++;
-        }
+        
    
 
     },
     computed: {
 
-        totalMarks(){
-            return parseInt(this.bangla) +  parseInt(this.english) +  parseInt(this.maths)
-        }
+        
 
     },
 })
