4cee10edb80663854ae8b9e071b3a9c6a427a982
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-03-22 07:45:05 +0600

    concept of custom directive

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
+Vue.directive('custom-directive',{
+    bind(el,binding,unbind){
+        console.log(el)
+        el.style.backgroundColor = 'blue';
+        el.style.fontSize = '20px';
+        el.title = 'title is changed';
+        console.log(binding)
+    },
+    update(object1,object2){
+        console.log('running update method')
+        var data = JSON.parse(JSON.stringify(object2));
+        console.log(object2)
+    },
+    unbind(){
 
+    }
+})
 
 
 var app = new Vue({
     el: '#our-app-1',
     data: {
-        
+        message: "this is custom call"
     },
 
     methods: {
 
-        btnClick(){
-            console.log(this.$refs)
-        },
+        changeValue(){
+            this.message = 'This is updated';
+        }
    
 
     },
